fa83c3caa3f6d94979d06e7d2ea5b59a2d33f12a
alanwsh <alan_wsh@hotmail.com>
2024-07-24 17:19:53 +0800

    Fixes navbar not showing on mobile ios

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -121,117 +121,119 @@ export default function NavBar() {
   };
 
   return (
-    <nav
-      className={classNames(
-        "mx-auto flex items-center justify-between p-1 pt-0 lg:px-8 w-full rounded-sm",
-        {
-          "bg-white": !state.dark,
-          "bg-black text-white": state.dark, // Adjust classes for dark mode
-        }
-      )}
-      aria-label="Global"
-      style={{ position: "sticky", top: 0, zIndex: 99 }}
-    >
-      <div className="flex">
-        <Link href="/" className="-m-1.5 p-1.5 flex items-center">
-          <Image
-            src="/logo.jpg"
-            alt="Logo"
-            className="h-[75%] w-auto rounded-full m-1"
-            priority
-            width={60}
-            height={60}
-          />
-          <span className="text-md font-semibold pl-2">ALAN</span>
-        </Link>
-      </div>
-      <div className="flex lg:hidden">
-        <button
-          type="button"
-          className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 dark:text-white text-gray-700"
-          onClick={toggleMenu}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    <>
+      <nav
+        className={classNames(
+          "mx-auto flex items-center justify-between p-1 pt-0 lg:px-8 w-full rounded-sm",
+          {
+            "bg-white": !state.dark,
+            "bg-black text-white": state.dark, // Adjust classes for dark mode
+          }
+        )}
+        aria-label="Global"
+        style={{ position: "sticky", top: 0, zIndex: 99 }}
+      >
+        <div className="flex">
+          <Link href="/" className="-m-1.5 p-1.5 flex items-center">
+            <Image
+              src="/logo.jpg"
+              alt="Logo"
+              className="h-[75%] w-auto rounded-full m-1"
+              priority
+              width={60}
+              height={60}
             />
-          </svg>
-        </button>
-      </div>
-      <div className="hidden lg:flex lg:gap-x-12 flex-1 justify-center">
-        {routes.map((route, index) => {
-          return route.href || route.onClick ? (
-            <Button
-              key={index}
-              className="text-md font-semibold leading-6 text-gray-900 dark:text-white"
-              style={{ textTransform: "none" }}
-              onClick={() => {
-                _handleMenuClick(index);
-              }}
+            <span className="text-md font-semibold pl-2">ALAN</span>
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 dark:text-white text-gray-700"
+            onClick={toggleMenu}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
             >
-              {route.title}
-            </Button>
-          ) : (
-            <React.Fragment key={index}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12 flex-1 justify-center">
+          {routes.map((route, index) => {
+            return route.href || route.onClick ? (
               <Button
+                key={index}
                 className="text-md font-semibold leading-6 text-gray-900 dark:text-white"
-                onClick={_handleClick}
                 style={{ textTransform: "none" }}
-                endIcon={<KeyboardArrowDown />}
+                onClick={() => {
+                  _handleMenuClick(index);
+                }}
               >
                 {route.title}
               </Button>
-              <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                sx={{ zIndex: 99 }}
-                onClose={_handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                }}
-              >
-                {route.menu?.list.map((menu: MenuItemType, i: number) => (
-                  <Link href={menu.href} passHref key={i}>
-                    <MenuItem onClick={_handleClose}>
-                      <div className="group relative flex items-center gap-x-6 p-4 text-sm">
-                        <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white dark:text-gray-900">
-                          {menu.icon}
+            ) : (
+              <React.Fragment key={index}>
+                <Button
+                  className="text-md font-semibold leading-6 text-gray-900 dark:text-white"
+                  onClick={_handleClick}
+                  style={{ textTransform: "none" }}
+                  endIcon={<KeyboardArrowDown />}
+                >
+                  {route.title}
+                </Button>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  sx={{ zIndex: 99 }}
+                  onClose={_handleClose}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  {route.menu?.list.map((menu: MenuItemType, i: number) => (
+                    <Link href={menu.href} passHref key={i}>
+                      <MenuItem onClick={_handleClose}>
+                        <div className="group relative flex items-center gap-x-6 p-4 text-sm">
+                          <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white dark:text-gray-900">
+                            {menu.icon}
+                          </div>
+                          <div className="flex-auto">
+                            <a
+                              href="#"
+                              className="block font-semibold text-gray-900 dark:text-white"
+                            >
+                              {menu.title}
+                              <span className="absolute inset-0"></span>
+                            </a>
+                            <p className="mt-1 text-gray-600 whitespace-normal dark:text-gray-300">
+                              {menu.description}
+                            </p>
+                          </div>
                         </div>
-                        <div className="flex-auto">
-                          <a
-                            href="#"
-                            className="block font-semibold text-gray-900 dark:text-white"
-                          >
-                            {menu.title}
-                            <span className="absolute inset-0"></span>
-                          </a>
-                          <p className="mt-1 text-gray-600 whitespace-normal dark:text-gray-300">
-                            {menu.description}
-                          </p>
-                        </div>
-                      </div>
-                    </MenuItem>
-                  </Link>
-                ))}
-              </Menu>
-            </React.Fragment>
-          );
-        })}
-      </div>
-      <div className="hidden lg:flex z-9999">
-        <ThemeToggle />
-      </div>
+                      </MenuItem>
+                    </Link>
+                  ))}
+                </Menu>
+              </React.Fragment>
+            );
+          })}
+        </div>
+        <div className="hidden lg:flex z-9999">
+          <ThemeToggle />
+        </div>
+      </nav>
       <Drawer
         open={isMenuOpen}
         onClose={toggleMenu}
@@ -239,7 +241,7 @@ export default function NavBar() {
         ModalProps={{
           keepMounted: true, // Better open performance on mobile.
         }}
-        style={{ zIndex: 99 }}
+        style={{ zIndex: 999 }}
       >
         <Box
           onKeyDown={toggleMenu}
@@ -331,6 +333,6 @@ export default function NavBar() {
           </List>
         </Box>
       </Drawer>
-    </nav>
+    </>
   );
 }
