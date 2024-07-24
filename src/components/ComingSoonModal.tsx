2bfc75d0b1c6a6fd4f4b82ea35b2a139b5dc30c0
alanwsh <alan_wsh@hotmail.com>
2024-07-24 17:13:23 +0800

    add animation to coming soon modal

--- src/components/ComingSoonModal.tsx
+++ src/components/ComingSoonModal.tsx
@@ -1,11 +1,17 @@
 import { useAppContext } from "@/context/app";
 import { Typography, Dialog, DialogContent } from "@mui/material";
 import Image from "next/image";
+import ThreeDotsWave from "./animation/ThreeDotsWave";
 
 export const ComingSoon = () => (
   <div>
     <Image src="/working_on.png" alt="Coming Soon" width={300} height={300} />
-    <Typography className="font-bold text-center">Coming Soon...</Typography>
+    <div className="flex justify-center">
+      <Typography className="font-bold text-center items-center mr-2">
+        Coming Soon
+      </Typography>
+      <ThreeDotsWave />
+    </div>
   </div>
 );
 export default function ComingSoonModal() {
