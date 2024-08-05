cf8573a6136943c105dab707b818a1b5b3917d06
alanwsh <alan_wsh@hotmail.com>
2024-08-05 15:57:10 +0800

    fix type error for mini game dock button

--- src/models/game.tsx
+++ src/models/game.tsx
@@ -1,6 +1,6 @@
 
 export type Game = {
-  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
   icon: string;
   name: string;
 };
